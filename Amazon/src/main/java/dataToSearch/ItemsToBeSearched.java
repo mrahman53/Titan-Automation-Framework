103b77f22d6b07669ed63362222b3d6783f93293
mrahman <rahmanww@gmail.com>
2016-01-09 10:54:27 -0500

    +fixed path

--- Amazon/src/main/java/dataToSearch/ItemsToBeSearched.java
+++ Amazon/src/main/java/dataToSearch/ItemsToBeSearched.java
@@ -15,7 +15,8 @@ public class ItemsToBeSearched  {
     //Option 2, supplu data from External source like excel files.
     DataReader dr = new DataReader();
     public String [] getData()throws IOException{
-        String [] st = dr.fileReader("C:\\Users\\rrt\\IdeaProjects\\Titan-Automation-Framework\\Amazon\\data\\file1.xls");
+        String path = System.getProperty("user.dir")+"/data/file1.xls";
+        String [] st = dr.fileReader(path);
      return st;
     }
 }
