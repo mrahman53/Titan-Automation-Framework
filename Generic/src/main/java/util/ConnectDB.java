a356b69e5d2c6341b85953282c2a62ea08e61b7b
mrahman <rahmanww@gmail.com>
2016-01-16 09:31:54 -0500

    modified connect db

--- Generic/src/main/java/util/ConnectDB.java
+++ Generic/src/main/java/util/ConnectDB.java
@@ -1,8 +1,11 @@
 package util;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  + * Created by mrahman on 12/22/15.
@@ -16,16 +19,35 @@ public class ConnectDB {
     private ResultSet resultSet = null;
     List<String> list = new ArrayList<String>();
 
+    public static Properties loadPropertiesFile()throws Exception{
+        Properties prop = new Properties();
+        InputStream ism = new FileInputStream("Generic/lib/MySQL.properties");
+        prop.load(ism);
+        ism.close();
+
+        return prop;
+    }
+
+    //Connect to Database
+    public void dbConnect() throws Exception{
+        Properties prop = loadPropertiesFile();
+        String driverClass = prop.getProperty("MYSQLJDBC.driver");
+        String url = prop.getProperty("MYSQLJDBC.url");
+        String userName = prop.getProperty("MYSQLJDBC.userName");
+        String passWord = prop.getProperty("MYSQLJDBC.passWord");
+        // This will load the MySQL driver.
+        Class.forName(driverClass);
+        // Setup the connection with the DB
+        connect = DriverManager.getConnection(url, userName, passWord);
+        System.out.println("Database connected");
+
+    }
+
     public List<String> readDataBase() throws Exception {
 
         try {
-// This will load the MySQL driver.
-            Class.forName("com.mysql.jdbc.Driver");
-// Setup the connection with the DB
-            connect = DriverManager.getConnection("jdbc:mysql://localhost/amazon?" +
-                    "user=root&password=MyNewPass");
-            System.out.println("Database connected");
 
+            dbConnect();
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
             // Result set get the result of the SQL query
